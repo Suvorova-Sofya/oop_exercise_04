fba49fba17598967e250841fff6f5e1d404903c9
Suvorova-Sofya <suvorova.sofya@yandex.ru>
2019-11-05 22:46:10 +0300

    изменено:      OOP_Lab4.docx 	изменено:      OOP_Lab4.pdf 	изменено:      Test1.txt 	изменено:      Test2.txt 	изменено:      Test3.txt 	изменено:      main.cpp

--- main.cpp
+++ main.cpp
@@ -10,40 +10,65 @@
 
 
 int main() {
-    five_angles<double> real_five_angles(std::cin);
-    print(real_five_angles);
-    std::tuple<point<double>, point<double>, point<double>,point<double>,point<double>>
-                                    fake_five_angles{{1, 2}, {2, -1}, {-3, -3}, {-4,0}, {-3,2}};
-    print(fake_five_angles);
-    std::cout << std::endl;
-    std::cout << center(real_five_angles)<<"\n";
-    std::cout << center(fake_five_angles) << "\n";
-    std::cout << square(real_five_angles)<<"\n";
-    std::cout << square(fake_five_angles);
-    std::cout << std::endl;
-    std::cout << std::endl;
-    six_angles<double> real_six_angles(std::cin);
-    print(real_six_angles);
-    std::tuple<point<double>, point<double>, point<double>,point<double>,point<double>,point<double>>
-            fake_six_angles{{1,2}, {2, -1}, {1, -3}, {-3,-3}, {-4,0},{-3,2}};
-    print(fake_six_angles);
-    std::cout << std::endl;
-    std::cout << center(real_six_angles)<<"\n";
-    std::cout << center(fake_six_angles) << "\n";
-    std::cout << square(real_six_angles)<<"\n";
-    std::cout << square(fake_six_angles);
-    std::cout << std::endl;
-    std::cout <<std::endl;
-    eight_angles<double> real_eight_angles(std::cin);
-    print(real_eight_angles);
-    std::tuple<point<double>, point<double>, point<double>,point<double>,point<double>,point<double>,point<double>,
-            point<double>> fake_eight_angles{{1, 2}, {2, -1}, {1, -3}, {0,-5}, {-2,-5},{-3,-3},{-4,0},{-3,2}};
-    print(fake_eight_angles);
-    std::cout << std::endl;
-    std::cout << center(real_eight_angles)<<"\n";
-    std::cout << center(fake_eight_angles) << "\n";
-    std::cout << square(real_eight_angles)<<"\n";
-    std::cout << square(fake_eight_angles);
-    std::cout << std::endl;
+    char str[12];
+    std::cin >> str;
+    if(strcmp("five_angles",str)==0) {
+        five_angles<double> real_five_angles(std::cin);
+        print(real_five_angles);
+        std::tuple<point<double>, point<double>, point<double>, point<double>, point<double>>
+                fake_five_angles{{1,  2},
+                                 {2,  -1},
+                                 {-3, -3},
+                                 {-4, 0},
+                                 {-3, 2}};
+        print(fake_five_angles);
+        std::cout << std::endl;
+        std::cout << center(real_five_angles) << "\n";
+        std::cout << center(fake_five_angles) << "\n";
+        std::cout << square(real_five_angles) << "\n";
+        std::cout << square(fake_five_angles);
+        std::cout << std::endl;
+    }else {
+        if(strcmp("six_angles",str)==0) {
+            six_angles<double> real_six_angles(std::cin);
+            print(real_six_angles);
+            std::tuple<point<double>, point<double>, point<double>, point<double>, point<double>, point<double>>
+                    fake_six_angles{{1,  2},
+                                    {2,  -1},
+                                    {1,  -3},
+                                    {-3, -3},
+                                    {-4, 0},
+                                    {-3, 2}};
+            print(fake_six_angles);
+            std::cout << std::endl;
+            std::cout << center(real_six_angles) << "\n";
+            std::cout << center(fake_six_angles) << "\n";
+            std::cout << square(real_six_angles) << "\n";
+            std::cout << square(fake_six_angles);
+            std::cout << std::endl;
+        }else{
+            if(strcmp("eight_angles",str)==0) {
+                eight_angles<double> real_eight_angles(std::cin);
+                print(real_eight_angles);
+                std::tuple<point<double>, point<double>, point<double>, point<double>, point<double>, point<double>, point<double>,
+                        point<double>> fake_eight_angles{{1,  2},
+                                                         {2,  -1},
+                                                         {1,  -3},
+                                                         {0,  -5},
+                                                         {-2, -5},
+                                                         {-3, -3},
+                                                         {-4, 0},
+                                                         {-3, 2}};
+                print(fake_eight_angles);
+                std::cout << std::endl;
+                std::cout << center(real_eight_angles) << "\n";
+                std::cout << center(fake_eight_angles) << "\n";
+                std::cout << square(real_eight_angles) << "\n";
+                std::cout << square(fake_eight_angles);
+                std::cout << std::endl;
+            }
+        }
+    }
+
     return 0;
 }
